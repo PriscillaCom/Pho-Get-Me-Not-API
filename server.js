7dee1a723fe9227de6d74da83f3d721bf3e8df66
Priscilla <priscillamoc97@gmail.com>
2021-07-14 00:35:46 -0700

    Add CartItems to post

--- server.js
+++ server.js
@@ -11,12 +11,14 @@ const database = {
       name: 'John',
       email: 'john@gmail.com',
       number: '636-342-3242',
+      cartItems: '',
       joined: new Date(),
     },
     {
       id: '124',
       name: 'Sally',
       email: 'sally@gmail.com',
+      cartItems: '',
       number: '636-566-3242',
       joined: new Date(),
     }
@@ -29,31 +31,30 @@ app.use(cors());
 
 
 app.get('/',(req,res) => {
-  console.log('database');
   res.send(database.users);
 });
 
 
 app.post("/checkout",(req,res) => {
   console.log('check me out');
-  const {name,email,number} = req.body;
+  const {name,email,number,cartItems} = req.body;
   database.users.push({
-      id: '126',
+      id: '127',
       name: name,
       email: email,
       number: number,
+      cartItems: cartItems,
       joined: new Date()
   });
-  res.json(database.users[database.users.length-1]);
+  res.send(database.users[database.length - 1]);
 });
 
 app.post("/order",(req,res) => {
-  if(req.body.number === database.users[0].number){
-    res.json('success');
-  }
-  else {
-    res.status(400).json('no such number');
-  }
+  database.users.forEach(user => {
+    if(user.number === req.body.number)
+      res.json('success');
+  });
+    res.status(400).json('no such number')
 })
 
 app.listen(3000,
